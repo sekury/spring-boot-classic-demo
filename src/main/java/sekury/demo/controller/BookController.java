71198389cd0a01764ccb329804bae68e2be60896
sekury <sergey.kuryugin@ya.ru>
2018-07-31 18:33:18 +0300

    Add book controller and view

---
+++ src/main/java/sekury/demo/controller/BookController.java
@@ -0,0 +1,20 @@
+package sekury.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sekury.demo.repository.BookRepository;
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
